37402ce73e2fda4795e984b483d1253bd14c466c
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2025-10-28 08:20:54 +0530

    commit after modifivations

--- src/pages/ServicesPage/WebDevelopmentSection/FelxibleImage/FelxibleImage.jsx
+++ src/pages/ServicesPage/WebDevelopmentSection/FelxibleImage/FelxibleImage.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
-import FooterSection from '../../../../sections/HomePageSection/FooterSection/FooterSection'
+  import CtaPanel from '../../../../components/CtaPanel/CtaPanel';
+  import FooterSection from '../../../../sections/HomePageSection/FooterSection/FooterSection';
 const FelxableImage = () => {
   return (
-    <>
-      this is felxable page
-       <FooterSection />
-    </>
+    <section>
+
+    
+
+<CtaPanel
+  title="KNOW MORE PROJECTS"
+  description="To know more projects and about all the details of the projects, the Technologies that we used to make the project successful in very short span of time please do contact us"
+  buttonLabel="Call us"
+  href="/contact"            // or omit href and pass onClick={() => ...}
+  align="center"             // or "left"
+  maxWidth={794}
+/>
+
+
+ <FooterSection
+        brand="Panacea IT Services"
+        year={new Date().getFullYear()}
+        services={[
+          "Software Development",
+          "Mobile Apps",
+          "UI/UX Design",
+          "Digital Marketing",
+          "SEO Optimization",
+          "Consulting",
+        ]}
+        company={[
+          "About Us",
+          "Our Team",
+          "Careers",
+          "Portfolio",
+          "Blog",
+          "Contact",
+        ]}
+        legal={["Privacy Policy", "Terms of Service", "Cookie Policy", "GDPR"]}
+      />
+
+    </section>
   )
 }
 
